ddd9f7ebf7174395bae512113130ceec7a2bbc0e
yordianvecino <yordian.vecino@hotmail.com>
2022-09-05 13:11:28 -0500

    Subo Cambio de CrudPerson

--- cohan-crud/src/components/CrudPerson.vue
+++ cohan-crud/src/components/CrudPerson.vue
@@ -1,20 +1,27 @@
 
 <template>
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
-
-    <div>
-        <h1>CRUD PRESON</h1>
-        <DataTable :value="person" :paginator="true" :rows="10">
-            <Column field="name" header="Name"></Column>
-            <Column field="phone" header="phone"></Column>
-            <Column field="email" header="email"></Column>
-        </DataTable>
+    <div style="margin: 0 auto; width: 80%">
+        <Panel header="CRUD PRESON">
+            <DataTable :value="person" :paginator="true" :rows="10">
+                <Column field="id_person" header="ID"></Column>
+                <Column field="name" header="Name"></Column>
+                <Column field="phone" header="phone"></Column>
+                <Column field="email" header="email"></Column>
+            </DataTable>
+        </Panel>
     </div>
 </template>
 
 <script>
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import Panel from 'primevue/panel';
 import PersonService from "../service/PersonService";
+
+import 'primevue/resources/themes/nova-light/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
 export default{
     name: 'CrudPerson',
     data(){
@@ -31,7 +38,12 @@ export default{
             this.person = data.data;
             console.log(this.person);
         });
-    }
+    },
+    components: {
+		DataTable,
+        Column,
+        Panel
+	}
 }
 </script>
 
